b84df287df0d55b1859d36605487a75aa8ec8d84
luxidea <contact@luxidea.net>
2016-09-22 04:01:01 +0900

    ユニットファイルを外部リポジトリへ整理。

--- FourierTimes.dpr
+++ FourierTimes.dpr
@@ -8,7 +8,13 @@ uses
   LUX in '_LIBRARY\LUXOPHIA\LUX\LUX.pas',
   LUX.D1 in '_LIBRARY\LUXOPHIA\LUX\LUX.D1.pas',
   LUX.FMX in '_LIBRARY\LUXOPHIA\LUX.FMX\LUX.FMX.pas',
-  LUX.D3 in '_LIBRARY\LUXOPHIA\LUX\LUX.D3.pas';
+  LUX.D3 in '_LIBRARY\LUXOPHIA\LUX\LUX.D3.pas',
+  LUX.DiscreteTrans.Fourier.D1 in '_LIBRARY\LUXOPHIA\LUX.Signal\LUX.DiscreteTrans.Fourier.D1.pas',
+  LUX.DiscreteTrans.D1 in '_LIBRARY\LUXOPHIA\LUX.Signal\LUX.DiscreteTrans.D1.pas',
+  LUX.Complex in '_LIBRARY\LUXOPHIA\LUX.Complex\LUX.Complex.pas',
+  LUX.Complex.D1 in '_LIBRARY\LUXOPHIA\LUX.Complex\LUX.Complex.D1.pas',
+  LUX.Audio.WAV in '_LIBRARY\LUXOPHIA\LUX.Audio\LUX.Audio.WAV.pas',
+  LUX.Audio in '_LIBRARY\LUXOPHIA\LUX.Audio\LUX.Audio.pas';
 
 {$R *.res}
 
